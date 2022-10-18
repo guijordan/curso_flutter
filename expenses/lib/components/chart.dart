6f8c17e2fe89d7771a1f8679bd71ca9d3e5b869b
Guilherme Jordan De Oliveira <guilherme.jordan@velp.com.br>
2022-10-17 22:09:21 -0300

    Criação do Wiget Chart (Gráfico).

---
+++ expenses/lib/components/chart.dart
@@ -0,0 +1,65 @@
+import 'package:expenses/components/chart_bar.dart';
+import 'package:expenses/models/transaction.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class Chart extends StatelessWidget {
+  const Chart(this.recentTransaction, {super.key});
+
+  final List<Transaction> recentTransaction;
+
+  List<Map<String, Object>> get groupedTransactions {
+    return List.generate(7, (index) {
+      final weekday = DateTime.now().subtract(Duration(days: index));
+      double totalSum = 0.0;
+
+      for (var i = 0; i < recentTransaction.length; i++) {
+        bool sameday = recentTransaction[i].date.day == weekday.day;
+        bool sameMonth = recentTransaction[i].date.month == weekday.month;
+        bool sameYear = recentTransaction[i].date.year == weekday.year;
+
+        if (sameday && sameMonth && sameYear) {
+          totalSum += recentTransaction[i].value;
+        }
+      }
+
+      return {
+        'day': DateFormat.E().format(weekday)[0],
+        'value': totalSum,
+      };
+    });
+  }
+
+  double get _weekTotalValue {
+    return groupedTransactions.fold(0.0, (sum, tr) {
+      return sum + (tr['value'] as double);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: const EdgeInsets.all(20),
+      // child: SingleChildScrollView(
+      //   scrollDirection: Axis.horizontal,
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupedTransactions.map((tr) {
+            return Flexible(
+              fit: FlexFit.tight,
+              child: ChartBar(
+                label: tr['day'] as String,
+                value: tr['value'] as double,
+                percentage: (tr['value'] as double) / _weekTotalValue,
+              ),
+            );
+          }).toList(),
+        ),
+      ),
+      // ),
+    );
+  }
+}
