cc6d3ff0f5c6d60e777c5a5516fb18397b8bc197
Guilherme Jordan De Oliveira <guilherme.jordan@velp.com.br>
2022-10-27 06:22:48 -0300

    Fim da aula sobre interfaces responsivas e adaptativas

---
+++ expenses/lib/components/adaptative_date_picker.dart
@@ -0,0 +1,61 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
+
+class AdaptativeDatePicker extends StatelessWidget {
+  final DateTime? selectedDate;
+  final Function(DateTime) onDateChanged;
+  const AdaptativeDatePicker({this.selectedDate, required this.onDateChanged, super.key});
+
+  _showDatePicker(BuildContext context) {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2019),
+      lastDate: DateTime.now(),
+    ).then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+      onDateChanged(pickedDate);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Platform.isIOS
+        ? Container(
+            height: 180,
+            child: CupertinoDatePicker(
+              mode: CupertinoDatePickerMode.date,
+              initialDateTime: DateTime.now(),
+              minimumDate: DateTime(2019),
+              maximumDate: DateTime.now(),
+              onDateTimeChanged: onDateChanged,
+            ),
+          )
+        : SizedBox(
+            height: 70,
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Text(
+                    selectedDate == null ? 'Nenhuma data selecionada!' : 'Data Selecionada: ${DateFormat('dd/MM/y').format(selectedDate!)}',
+                  ),
+                ),
+                TextButton(
+                  child: const Text(
+                    'Selecionar Data',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  onPressed: () => _showDatePicker(context),
+                )
+              ],
+            ),
+          );
+  }
+}
