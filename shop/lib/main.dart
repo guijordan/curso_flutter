08cf34a40ed7ae9ae4846f7273e766d80a9ce754
Guilherme Jordan De Oliveira <guilherme.jordan@velp.com.br>
2022-11-15 18:40:21 -0300

    Uso da biblioteca 'Provider' que serve para gerenciamento de estado. Uso das classes 'ChangeNotifierProvider' do mixin 'ChangeNotifier' e a obtenção do objeto gerenciado pelo provider através do método: Provider.of<T>(context) sendo que T é uma classe que usa o mixin 'ChangeNotifier'

--- shop/lib/main.dart
+++ shop/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/product_list.dart';
 import 'package:shop/pages/counter_page.dart';
 import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
@@ -15,21 +17,24 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        colorScheme: ThemeData().colorScheme.copyWith(
-              primary: Colors.purple,
-              secondary: Colors.deepOrange,
-            ),
-        fontFamily: 'Lato',
+    return ChangeNotifierProvider(
+      create: (_) => ProductList(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          colorScheme: ThemeData().colorScheme.copyWith(
+                primary: Colors.purple,
+                secondary: Colors.deepOrange,
+              ),
+          fontFamily: 'Lato',
+        ),
+        home: const ProductsOverviewPage(),
+        routes: {
+          AppRoutes.PRODUCT_DETAIL: (ctx) => const ProductDetailPage(),
+        },
+        debugShowCheckedModeBanner: false,
       ),
-      home: ProductsOverviewPage(),
-      routes: {
-        AppRoutes.PRODUCT_DETAIL: (ctx) => const ProductDetailPage(),
-      },
-      debugShowCheckedModeBanner: false,
     );
   }
 }
