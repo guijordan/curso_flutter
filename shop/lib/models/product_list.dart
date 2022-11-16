c27caa631062258a15415556f834c4de0f4fee65
Guilherme Jordan De Oliveira <guilherme.jordan@velp.com.br>
2022-11-16 06:28:20 -0300

    Controlando a exibição de produtos favoritos de forma global ou seja todas as telas que utilizarem o provider ProductList vai filtrar os produtos favoritos se assim selecionado pelo usuário.

--- shop/lib/models/product_list.dart
+++ shop/lib/models/product_list.dart
@@ -5,7 +5,24 @@ import 'package:shop/models/product.dart';
 class ProductList with ChangeNotifier {
   final List<Product> _items = dummyProducts;
 
-  List<Product> get items => [..._items];
+  List<Product> get items {
+    if (_showFavoriteOnly) {
+      return _items.where((prod) => prod.isFavorite).toList();
+    }
+    return [..._items];
+  }
+
+  bool _showFavoriteOnly = false;
+
+  void showFavoriteOnly() {
+    _showFavoriteOnly = true;
+    notifyListeners();
+  }
+
+  void showAll() {
+    _showFavoriteOnly = false;
+    notifyListeners();
+  }
 
   void addProduct(Product product) {
     _items.add(product);
