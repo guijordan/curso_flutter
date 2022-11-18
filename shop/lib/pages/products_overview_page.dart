538dd5c0e7b53a588c9c8e8788b29868f6b63695
Guilherme Jordan De Oliveira <guilherme.jordan@velp.com.br>
2022-11-18 07:24:14 -0300

    Uso dos componentes MultiProvider quando você precisa usar mais de um provider para mais de uma lista ou objeto raiz. Veja um exemplo de uso:
    return MultiProvider(
          providers: [
            ChangeNotifierProvider(
              create: (_) => ProductList(),
            ),
            ChangeNotifierProvider(
              create: (_) => Cart(),
            ),
          ],
          child: MaterialApp(
            title: 'Flutter Demo',
            theme: ThemeData(
              primarySwatch: Colors.purple,
              colorScheme: ThemeData().colorScheme.copyWith(
                    primary: Colors.purple,
                    secondary: Colors.deepOrange,
                  ),
              fontFamily: 'Lato',
            ),
            home: const ProductsOverviewPage(),
            routes: {
              AppRoutes.PRODUCT_DETAIL: (ctx) => const ProductDetailPage(),
              AppRoutes.CART: (ctx) => const CartPage(),
            },
            debugShowCheckedModeBanner: false,
          ),
        );
    
     Para recuperar valores do provider. Um pra uma instancia de Cart e
     outro para instância de ProductList
    
     final product = Provider.of<Product>(context, listen: false);
        final cart = Provider.of<Cart>(context, listen: false);
    
    Uso do componente chip. Lembra um label com bordas arredondadas.
    exemplo de uso:
    
    Chip(
                        backgroundColor: Theme.of(context).colorScheme.primary,
                        label: Text(
                          'R\$${cart.totalAmount}',
                          style: TextStyle(
                            color: Theme.of(context).primaryTextTheme.headline6?.color,
                          ),
                        ),
                      ),

--- shop/lib/pages/products_overview_page.dart
+++ shop/lib/pages/products_overview_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop/components/badge.dart';
 import 'package:shop/components/product_grid.dart';
-import 'package:shop/models/product_list.dart';
+import 'package:shop/models/cart.dart';
+import 'package:shop/utils/app_routes.dart';
 
 enum FilterOptions {
   favorite,
@@ -46,6 +48,18 @@ class _ProductsOverviewPageState extends State<ProductsOverviewPage> {
                 print(_showFavoriteOnly);
               });
             },
+          ),
+          Consumer<Cart>(
+            child: IconButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed(AppRoutes.CART);
+              },
+              icon: const Icon(Icons.shopping_cart),
+            ),
+            builder: (ctx, cart, child) => Badge(
+              value: cart.itemsCount.toString(),
+              child: child!,
+            ),
           ),
         ],
       ),
