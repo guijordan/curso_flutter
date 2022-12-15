7f7dcb1e2323d4b23e06f00dba8c496441fec1a9
Guilherme Jordan De Oliveira <guilherme.jordan@velp.com.br>
2022-12-15 07:15:20 -0300

    Uso da biblioteca shared_preferences para persistir dados da aplicação mesmo se a aplicação for fechada ou smartphone reiniciado. O mecanismo de salvamento é através de chave e valor semelhante ao localStorage de um navegador web.
    Site da biblioteca: https://pub.dev/packages/shared_preferences

--- shop/lib/models/auth.dart
+++ shop/lib/models/auth.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:shop/data/store.dart';
 import 'package:shop/exceptions/auth_exception.dart';
 
 class Auth with ChangeNotifier {
@@ -48,6 +49,14 @@ class Auth with ChangeNotifier {
       _email = body['email'];
       _userId = body['localId'];
       _expiryDate = DateTime.now().add(Duration(seconds: int.parse(body['expiresIn'])));
+
+      Store.saveMap('userData', {
+        'token': _token,
+        'email': _email,
+        'userId': _userId,
+        'expiryDate': _expiryDate!.toIso8601String(),
+      });
+
       _autoLogout();
       notifyListeners();
     }
@@ -61,13 +70,33 @@ class Auth with ChangeNotifier {
     return _autenticate(email, password, 'signInWithPassword');
   }
 
+  Future<void> tryAutoLogin() async {
+    if (isAuth) return;
+
+    final userData = await Store.getMap('userData');
+    if (userData.isEmpty) return;
+
+    final expiryDate = DateTime.parse(userData['expiryDate']);
+    if (expiryDate.isBefore(DateTime.now())) return;
+
+    _token = userData['token'];
+    _email = userData['email'];
+    _userId = userData['userId'];
+    _expiryDate = expiryDate;
+
+    _autoLogout();
+    notifyListeners();
+  }
+
   void logout() {
     _token = null;
     _email = null;
     _userId = null;
     _expiryDate = null;
     _clearLogoutTimer();
-    notifyListeners();
+    Store.remove('userData').then((value) {
+      notifyListeners();
+    });
   }
 
   void _clearLogoutTimer() {
