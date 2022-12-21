f6861662a71a830ac7b39d478a5084ead7e4aeb2
Guilherme Jordan De Oliveira <guilherme.jordan@velp.com.br>
2022-12-21 05:34:28 -0300

    Aula sobre o uso da câmera do dispositivo utilizando a biblioteca image_picker escrita em flutter.

--- great_places/lib/widgets/image_input.dart
+++ great_places/lib/widgets/image_input.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   const ImageInput({super.key});
@@ -8,6 +11,24 @@ class ImageInput extends StatefulWidget {
 }
 
 class _ImageInputState extends State<ImageInput> {
+  File? _storedImage;
+
+  _takePicture() async {
+    final ImagePicker picker = ImagePicker();
+    XFile? imageFile = await picker.pickImage(
+      source: ImageSource.camera,
+      maxWidth: 600,
+    );
+
+    if (imageFile == null) {
+      return;
+    }
+
+    setState(() {
+      _storedImage = File(imageFile.path);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -22,14 +43,14 @@ class _ImageInputState extends State<ImageInput> {
             ),
           ),
           alignment: Alignment.center,
-          child: const Text('Nenhuma Imagem !!'),
+          child: _storedImage != null ? Image.file(_storedImage!, width: double.infinity, fit: BoxFit.cover) : const Text('Nenhuma Imagem !!'),
         ),
         const SizedBox(width: 10),
         Expanded(
           child: TextButton.icon(
             icon: const Icon(Icons.camera),
             label: const Text('Tirar Foto'),
-            onPressed: () {},
+            onPressed: _takePicture,
             style: TextButton.styleFrom(
               textStyle: TextStyle(
                 color: Theme.of(context).colorScheme.primary,
