af473de2fcb3bba8a8fa89917e5fc7ec49b3046e
Guilherme Jordan De Oliveira <guilherme.jordan@velp.com.br>
2022-12-21 06:23:09 -0300

    Uso do provider para atualizar a lista de lugares 'Places' e sempre que a lista mudar os componentes serão atualizados.
    Configuração necessária para uso de cêmera no iOS.
    
    ver diff do arquivo info.plist.

--- great_places/lib/providers/great_places.dart
+++ great_places/lib/providers/great_places.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:great_places/models/place.dart';
 
@@ -13,5 +16,17 @@ class GreatPlaces with ChangeNotifier {
 
   Place itemByIndex(int index) {
     return _items[index];
+  }
+
+  void addPlace(String titulo, File image) {
+    final newPlace = Place(
+      id: Random().nextDouble().toString(),
+      title: titulo,
+      location: null,
+      image: image,
+    );
+
+    _items.add(newPlace);
+    notifyListeners();
   }
 }
