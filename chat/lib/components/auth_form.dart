9da1352a2e0cd08396ec65f64c2a3d99340d8467
Guilherme Jordan De Oliveira <guilherme.jordan@velp.com.br>
2022-12-29 20:05:10 -0300

    Inicio da criação do formulário de login e cadastro de novo usuário

---
+++ chat/lib/components/auth_form.dart
@@ -0,0 +1,68 @@
+import 'package:chat/models/auth_form_data.dart';
+import 'package:flutter/material.dart';
+
+class AuthForm extends StatefulWidget {
+  const AuthForm({super.key});
+
+  @override
+  State<AuthForm> createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _formData = AuthFormData();
+
+  void _submit() {
+    _formKey.currentState?.validate();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.all(20),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              if (_formData.isSignup)
+                TextFormField(
+                  key: const ValueKey('name'),
+                  initialValue: _formData.name,
+                  onChanged: (name) => _formData.name = name,
+                  decoration: const InputDecoration(labelText: 'Nome'),
+                ),
+              TextFormField(
+                key: const ValueKey('email'),
+                initialValue: _formData.name,
+                onChanged: (email) => _formData.email = email,
+                decoration: const InputDecoration(labelText: 'E-mail'),
+              ),
+              TextFormField(
+                key: const ValueKey('password'),
+                obscureText: true,
+                initialValue: _formData.name,
+                onChanged: (password) => _formData.password = password,
+                decoration: const InputDecoration(labelText: 'Senha'),
+              ),
+              const SizedBox(height: 12),
+              ElevatedButton(
+                onPressed: _submit,
+                child: Text(_formData.isLogin ? 'Entrar' : 'Cadastrar'),
+              ),
+              TextButton(
+                child: Text(_formData.isLogin ? 'Criar uma nova conta?' : 'Já possui conta?'),
+                onPressed: () {
+                  setState(() {
+                    _formData.toggleAuthMode();
+                  });
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
