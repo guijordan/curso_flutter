f9ff2a012e3e138422e27b7042c8f3f8644ca6b8
Guilherme Jordan De Oliveira <guilherme.jordan@velp.com.br>
2023-01-05 07:24:02 -0300

    Criação do notification service
    Criação da página de notificação
    
    Criação do icone de notificação
    
    Configuração do firebase para android e ios.

--- chat/lib/pages/chat_page.dart
+++ chat/lib/pages/chat_page.dart
@@ -1,7 +1,13 @@
+import 'dart:math';
+
 import 'package:chat/components/messages.dart';
 import 'package:chat/components/new_message.dart';
+import 'package:chat/core/models/chat_notification.dart';
 import 'package:chat/core/services/auth/auth_service.dart';
+import 'package:chat/core/services/notification/chat_notification_service.dart';
+import 'package:chat/pages/notification_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ChatPage extends StatelessWidget {
   const ChatPage({super.key});
@@ -9,10 +15,11 @@ class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Cod3r Chat'),
-          actions: [
-            DropdownButton(
+      appBar: AppBar(
+        title: const Text('Cod3r Chat'),
+        actions: [
+          DropdownButtonHideUnderline(
+            child: DropdownButton(
               icon: Icon(
                 Icons.more_vert,
                 color: Theme.of(context).primaryIconTheme.color,
@@ -23,12 +30,12 @@ class ChatPage extends StatelessWidget {
                   child: Container(
                     child: Row(
                       children: [
-                        Icon(
+                        const Icon(
                           Icons.exit_to_app,
                           color: Colors.black87,
                         ),
-                        SizedBox(width: 10),
-                        Text('Sair'),
+                        const SizedBox(width: 10),
+                        const Text('Sair'),
                       ],
                     ),
                   ),
@@ -40,17 +47,60 @@ class ChatPage extends StatelessWidget {
                 }
               },
             ),
-          ],
-        ),
-        body: SafeArea(
-          child: Column(
-            children: const [
-              Expanded(
-                child: Messages(),
+          ),
+          Stack(
+            children: [
+              IconButton(
+                icon: const Icon(Icons.notifications),
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (ctx) {
+                        return const NotificationPage();
+                      },
+                    ),
+                  );
+                },
               ),
-              NewMessage(),
+              Positioned(
+                top: 5,
+                right: 5,
+                child: CircleAvatar(
+                  maxRadius: 10,
+                  backgroundColor: Colors.red.shade800,
+                  child: Text(
+                    "${Provider.of<ChatNotificationService>(context).itemsCount}",
+                    style: const TextStyle(
+                      fontSize: 12,
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
-        ));
+        ],
+      ),
+      body: SafeArea(
+        child: Column(
+          children: const [
+            Expanded(
+              child: Messages(),
+            ),
+            NewMessage(),
+          ],
+        ),
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   child: Icon(Icons.add),
+      //   onPressed: () {
+      //     Provider.of<ChatNotificationService>(context, listen: false).add(
+      //       ChatNotification(
+      //         title: "Mais uma nofitificação",
+      //         body: Random().nextDouble().toString(),
+      //       ),
+      //     );
+      //   },
+      // ),
+    );
   }
 }
