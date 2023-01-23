8789c2f3ff8086c0facc3681e3b9b23bf25d0f11
Guilherme Jordan De Oliveira <guilherme.jordan@velp.com.br>
2023-01-23 19:47:01 -0300

    Registro das imagns do aplicativo Implementação do componente CampoWidget que representa um quadradinho do jogo campo minado.

--- campo_minado/lib/screens/campo_minado_app.dart
+++ campo_minado/lib/screens/campo_minado_app.dart
@@ -1,4 +1,7 @@
+import 'package:campo_minado/components/campo_widget.dart';
 import 'package:campo_minado/components/resultado_widget.dart';
+import 'package:campo_minado/models/campo.dart';
+import 'package:campo_minado/models/explosao_exception.dart';
 import 'package:flutter/material.dart';
 
 class CampoMinadoApp extends StatelessWidget {
@@ -6,8 +9,28 @@ class CampoMinadoApp extends StatelessWidget {
     print('reiniciar...');
   }
 
+  void _abrir(Campo campo) {
+    print('abrir...');
+  }
+
+  void _alternarMarcacao(Campo campo) {
+    print('alternar Marcacao');
+  }
+
   @override
   Widget build(BuildContext context) {
+    Campo vizinho1 = Campo(linha: 1, coluna: 0);
+    vizinho1.minar();
+    Campo vizinho2 = Campo(linha: 1, coluna: 1);
+    vizinho2.minar();
+    Campo campo = Campo(linha: 0, coluna: 0);
+    campo.adicionarVizinho(vizinho1);
+    campo.adicionarVizinho(vizinho2);
+    try {
+      //campo.minar();
+      campo.alternarMarcacao();
+    } on ExplosaoException {}
+
     return MaterialApp(
       home: Scaffold(
         appBar: ResultadoWidget(
@@ -15,7 +38,11 @@ class CampoMinadoApp extends StatelessWidget {
           venceu: true,
         ),
         body: Container(
-          child: Text('Tabuleiro'),
+          child: CampoWidget(
+            campo: campo,
+            onAbrir: _abrir,
+            onAlterarMarcacao: _alternarMarcacao,
+          ),
         ),
       ),
     );
