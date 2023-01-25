f78043c18d839975f9a8406db3b32da81ce6dd5b
Guilherme Jordan De Oliveira <guilherme.jordan@velp.com.br>
2023-01-24 22:55:58 -0300

    Projeto Calculadora do curso flutter parte I - Implementação da interface de usuário. A parte II será a lógica dos botões.

---
+++ calculator2/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:calculator2/screens/calculator.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Calculator());
+}
---
+++ calculator2/lib/screens/calculator.dart
@@ -0,0 +1,33 @@
+import 'package:calculator2/components/display.dart';
+import 'package:calculator2/components/keyboard.dart';
+import 'package:calculator2/models/memory.dart';
+import 'package:flutter/material.dart';
+
+class Calculator extends StatefulWidget {
+  const Calculator({super.key});
+
+  @override
+  State<Calculator> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  final Memory memory = Memory();
+
+  _onPressed(String command) {
+    setState(() {
+      memory.applyCommand(command);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Column(
+        children: [
+          Display(text: memory.value),
+          Keyboard(_onPressed),
+        ],
+      ),
+    );
+  }
+}
