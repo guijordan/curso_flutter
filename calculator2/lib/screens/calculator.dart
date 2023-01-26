e96cbd05554c9ee87e1cb987eb8dfe930ed0aafb
Guilherme Jordan De Oliveira <guilherme.jordan@velp.com.br>
2023-01-26 19:30:18 -0300

    Fim Projeto calculadora feito em flutter.

--- calculator2/lib/screens/calculator.dart
+++ calculator2/lib/screens/calculator.dart
@@ -2,6 +2,7 @@ import 'package:calculator2/components/display.dart';
 import 'package:calculator2/components/keyboard.dart';
 import 'package:calculator2/models/memory.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Calculator extends StatefulWidget {
   const Calculator({super.key});
@@ -21,6 +22,8 @@ class _CalculatorState extends State<Calculator> {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
     return MaterialApp(
       home: Column(
         children: [
