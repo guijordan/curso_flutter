03a94d0afc5d9a7f7c15fc4f457e1f4f56c155e9
Guilherme Jordan De Oliveira <guilherme.jordan@velp.com.br>
2023-01-30 19:26:45 -0300

    Implementação componente Cronometro e BotaoCronometro

--- pomodoro/lib/components/pomodoro.dart
+++ pomodoro/lib/components/pomodoro.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pomodoro/components/EntradaTempo.dart';
+import 'package:pomodoro/components/cronometro.dart';
 
 class Pomodoro extends StatelessWidget {
   const Pomodoro({super.key});
@@ -8,20 +9,24 @@ class Pomodoro extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Text('Pomodoro'),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              EntradaTempo(
-                titulo: 'Trabalho',
-                valor: 25,
-              ),
-              EntradaTempo(
-                titulo: 'Descanso',
-                valor: 5,
-              ),
-            ],
+          Expanded(child: Cronometro()),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 40),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                EntradaTempo(
+                  titulo: 'Trabalho',
+                  valor: 25,
+                ),
+                EntradaTempo(
+                  titulo: 'Descanso',
+                  valor: 5,
+                ),
+              ],
+            ),
           ),
         ],
       ),
